f2805e43ad74acc1f39707ae077c3208c745e69a
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-02-28 21:05:10 -0500

    Got styling working for text bubbles

--- app/components/DashboardNavigator/ChatScreen/MessagesList/MessageRow/Component.js
+++ app/components/DashboardNavigator/ChatScreen/MessagesList/MessageRow/Component.js
@@ -7,21 +7,26 @@ import moment from 'moment'
 import styles from './Styles'
 import translations from '../../../../../i18n'
 
-const MESSAGE_TEXT_MARGIN = 0
+const MESSAGE_TEXT_MARGIN = 50
 
 const MessageRowComponent = props => {
   const isCurrentUser = props.isCurrentUser
   const alignItems = 'flex-end'
+  const margin = isCurrentUser ? {marginLeft: MESSAGE_TEXT_MARGIN} : {marginRight: MESSAGE_TEXT_MARGIN}
   const username = isCurrentUser ? translations.t('you') : props.message.user.email
-  const relDate = relativeDate(new Date(props.message.createdAt))
-  const date = moment(props.message.createdAt).format('dddd, L [at] h:mma')
+  const date = moment(props.message.createdAt).format('dddd, L')
   return (
     <View style={styles.container}>
-      <View style={ [styles.bubbleView, {alignItems}] }>
-        <Text style={styles.userText}>Withdrawl: $20.00 Running Balance: $1,500</Text>
-        <Text style={styles.userText}>{relDate} - {date}</Text>
-        <Text style={styles.userText}>{translations.t('notes')}: {props.message.text}</Text>
-        <Text style={styles.userText}>{username}</Text>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'white'}}>
+        <View style={{width: '55%', height: 75, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', padding: 10}}>
+          <Text style={{fontSize: 12, paddingBottom: 10}}>{username}</Text>
+          <Text style={{fontSize: 12}} numberOfLines={2}>{props.message.text}</Text>
+        </View>
+        <View style={{width: '45%', height: 75, flexDirection: 'column', alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10}}>
+          <Text style={{fontSize: 12, paddingBottom: 10}}>{date}</Text>
+          <Text style={{fontSize: 12}}>Withdrawl: $20.00</Text>
+          <Text style={{fontSize: 12}}>Running Balance: $1,500</Text>
+        </View>
       </View>
     </View>
   )
