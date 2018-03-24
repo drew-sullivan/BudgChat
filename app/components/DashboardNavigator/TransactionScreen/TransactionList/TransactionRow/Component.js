751224d1e54478a240f5134a4b9cf12e4c83bb6f
Drew Sullivan <drew.sullivan.dma@gmail.com>
2018-03-23 20:49:39 -0400

    WIP: adding recently-added props to replace sample data

--- app/components/DashboardNavigator/TransactionScreen/TransactionList/TransactionRow/Component.js
+++ app/components/DashboardNavigator/TransactionScreen/TransactionList/TransactionRow/Component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import PropTypes from 'prop-types'
+
+import NumberFormat from 'react-number-format'
 import relativeDate from 'relative-date'
 import moment from 'moment'
 
@@ -8,15 +10,18 @@ import styles from './Styles'
 
 const TransactionRowComponent = props => {
   const isCurrentUser = props.isCurrentUser
-  // const username = isCurrentUser ? 'You' : props.message.user.email
   const username = isCurrentUser ? 'You' : props.transaction.user.email
-  // const date = moment(props.message.createdAt).format('dddd, L')
   const date = moment(props.transaction.createdAt).format('dddd, L')
+  const inputValue = props.transaction.inputValue
+  const transactionType = inputValue < 0 ? 'Withdrawl' : 'Deposit'
   return (
     <View style={styles.container}>
       <View style={[styles.box, styles.leftBox]}>
-        <Text style={styles.textBig}>Withdrawl:
-          <Text style={styles.textBold}> $20.00</Text>
+        <Text style={styles.textBig}>{transactionType}:
+          {/* <Text style={styles.textBold}> {inputValue}</Text> */}
+          <Text style={styles.textBold}>
+            <NumberFormat value={2456981} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+          </Text>
         </Text>
         <Text style={styles.regularText}>Running Balance:
           <Text style={styles.textBold}> $1,500</Text>
@@ -25,8 +30,7 @@ const TransactionRowComponent = props => {
       <View style={[styles.box, styles.rightBox]}>
         <Text style={styles.regularText}>{date}</Text>
         <Text style={styles.regularText} numberOfLines={1}>Notes:
-          {/* <Text style={styles.textBold}> {props.message.text}</Text> */}
-          <Text style={styles.textBold}> {props.transaction.inputValue}</Text>
+          <Text style={styles.textBold}>[notes]</Text>
         </Text>
         <Text style={styles.regularText}>{username}</Text>
       </View>
@@ -36,18 +40,11 @@ const TransactionRowComponent = props => {
 
 TransactionRowComponent.propTypes = {
   isCurrentUser: PropTypes.bool.isRequired,
-  // message: PropTypes.shape({
-  //   createdAt: PropTypes.number.isRequired,
-  //   text: PropTypes.string.isRequired,
-  //   user: PropTypes.shape({
-  //     email: PropTypes.string.isRequired
-  //   })
-  // }),
   transaction: PropTypes.shape({
     createdAt: PropTypes.number.isRequired,
     inputValue: PropTypes.number.isRequired,
     user: PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       email: PropTypes.string.isRequired
     })
   })
